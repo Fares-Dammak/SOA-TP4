d3f2bf7da8842a36dd9fd57486af93ae2ff43df4
Fares DAMMAK <dammak.fares28@gmail.com>
2023-11-05 22:42:14 +0100

    Add files via upload

---
+++ Fares_Dammak_TP31/src/main/java/sever/ServeurJWS.java
@@ -0,0 +1,11 @@
+package sever;
+
+        import jakarta.xml.ws.Endpoint;
+        import service.CalculatriceWS;
+
+public class ServeurJWS {
+    public static void main(String[] args) {
+        String url=" http://localhost:8084/";
+        Endpoint.publish(url,new CalculatriceWS());
+    }
+}
